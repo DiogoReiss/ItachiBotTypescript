7a0fcd354189d2281a9cb1bf935e98a065746687
Sensh1 <diogommreis1@gmail.com>
2020-08-10 01:54:41 +0100

    creating a readme file and modifying help command

---
+++ readme.md
@@ -0,0 +1,18 @@
+# Itachi Flamenguista Bot
+Itachi Flamenguista is a Discord Bot with funny commands
+
+## Installation
+Use yarn or npm to install the right stuffs
+```bash
+npm install -i
+```
+or
+```bash
+yarn install -i
+```
+## How to bring life to this humble bot?
+Normaly i use yarn but you can use npm too
+```bash
+yarn knex:migrate
+yarn develop
+```
--- src/commands/help.ts
+++ src/commands/help.ts
@@ -11,13 +11,14 @@ export default class CommandList {
       .setTitle('Lista de Comandos')
       .setAuthor('Itachi Flamenguista')
       .setDescription('Ouvi que alguem pediu uma ajuda aqui')
-      .setThumbnail('')
+      .setThumbnail('https://raw.githubusercontent.com/DiogoReiss/ItachiBotTypescript/master/public/itachiflamenguista2.jpeg')
       .addFields(
         { name: 'Nosso prefixo', value: 'O prefixo do nosso bot é: i., exemplo: i.comando' },
         { name: 'i.anao', value: 'tenha cuidado ao invocar esse cantigo, iremos reviver um anão no chat!' },
         { name: 'i.bot', value: 'temos um vasto estoque de indianos gentis que irão te responder na hora!' },
         { name: 'EM CONSTRUÇÃO', value: 'Nosso incrivel e amado mestre está desenvolvendo um incrivel minigame' }
       )
+      .setFooter('Itachi Flamenguista Bot', 'https://raw.githubusercontent.com/DiogoReiss/ItachiBotTypescript/master/public/itachiflamenguista.jpg')
   }
 
   sendHelpMessage(msg: any) {
